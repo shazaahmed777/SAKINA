40a2c4ae4040d8240fcf3e30f61cf7212f4ccb45
nashwa6samy6-arch <nashwa6samy6@gmail.com>
2026-04-17 17:58:32 +0200

    edit custom appbar

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sakina/core/theme/app_colors.dart';
 
 class Myappbar extends StatelessWidget implements PreferredSizeWidget {
@@ -6,17 +7,58 @@ class Myappbar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: const Text('Home Page'),
-      backgroundColor: AppColors.appbarColor,
-      leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-      actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
-        IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
-      ],
+    return Container(
+      height: 80.h,
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      decoration: BoxDecoration(
+        color: AppColors.appbarColor,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      ),
+      child: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center, // ✅ centers vertically
+          children: [
+            
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 24.r,
+                  backgroundColor: AppColors.primaryBeig,
+                  backgroundImage: NetworkImage(
+                    'https://thumbs.dreamstime.com/b/avatar-profile-icon-flat-style-female-user-vector-illustration-isolated-background-women-sign-business-concept-321407993.jpg',
+                  ),
+                ),
+                SizedBox(width: 10.w),
+                Text(
+                  'Basmala',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Manrope',
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+
+            // Notification icon
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.notifications),
+              padding: EdgeInsets.zero,
+              constraints: const BoxConstraints(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(80.h);
 }
